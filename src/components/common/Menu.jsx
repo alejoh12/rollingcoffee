1c5f05fe852dc077a9072c0ad7da3bd07030343c
Alejo Herrera <alejoherrera1166@gmail.com>
2024-04-08 11:13:03 -0300

    Agregué la estetica del navbar

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../../assets/Coffee_Logo.png";
 
 const Menu = () => {
-    return (
-        <div>
-            <p>aqui va el navbar</p>
-        </div>
-    );
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            alt="El logo de nuestra empresa."
+            className="img-fluid"
+            width={150}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Administrador</Nav.Link>
+            <Nav.Link href="#link">Registro</Nav.Link>
+            <Nav.Link href="#link">Inicio Sesión</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Menu;
