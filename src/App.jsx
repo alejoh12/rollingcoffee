f5c5b2412e36a9d8219a005f3379beaf58cf1fb3
Alejo Herrera <alejoherrera1166@gmail.com>
2024-04-08 10:16:07 -0300

    Footer creado.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer.jsx";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      
+      <Container className="my-4 mainPage"></Container>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="py-3 bg-dark text-light text-center">
+      <p className="container">
+        &copy; Facundo Alejo Herrera - Todos los derechos reservados.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
