d7c7ae581f05e30bb86212c4709e3714ffba9c99
Alejo Herrera <alejoherrera1166@gmail.com>
2024-04-09 16:58:25 -0300

    Titulo y boton para agregar productos agregado.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/common/Footer.jsx";
 import Menu from "./components/common/Menu.jsx";
 import Inicio from "./components/pages/Inicio.jsx";
+import Admin from "./components/pages/Admin.jsx";
 
 function App() {
   return (
     <>
       <div className="mainPage">
         <Menu></Menu>
-        <Inicio></Inicio>
+        {/* <Inicio></Inicio> */}
+        <Admin></Admin>
       </div>
       <Footer></Footer>
     </>
--- src/components/pages/Admin.jsx
+++ src/components/pages/Admin.jsx
@@ -2,7 +2,44 @@ import { Container, Table, Row, Button } from "react-bootstrap";
 
 const Admin = () => {
   return (
-    <></>
+    <Container className="my-3">
+      <div className="d-flex justify-content-between">
+        <h1 className="display-4">Productos Disponibles</h1>
+        <div className="pt-4">
+          <Button variant="primary">Agregar</Button>
+        </div>
+      </div>
+      <hr />
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
   );
 };
 
