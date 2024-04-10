ec62399e19af9e2897a61b53c7ebc7e488c00178
Alejo Herrera <alejoherrera1166@gmail.com>
2024-04-10 15:57:28 -0300

    Agregué dependencia react hook form.

--- src/components/pages/Admin.jsx
+++ src/components/pages/Admin.jsx
@@ -1,15 +1,16 @@
 import { Container, Button } from "react-bootstrap";
 import TablaProductosAdmin from "./producto/TablaProductosAdmin";
+import { Link } from "react-router-dom";
 
 const Admin = () => {
   return (
     <Container className="my-3">
       <div className="d-flex justify-content-between">
         <h1 className="display-4">Productos Disponibles</h1>
-        <div className="pt-4">
-          <Button variant="primary" to="/administrador/agregar">
-          <i className="bi bi-file-earmark-plus"></i>
-          </Button>
+        <div className="pt-3">
+          <Link variant="primary" to="/administrador/agregar">
+          <i className="bi bi-file-earmark-plus display-6"></i>
+          </Link>
         </div>
       </div>
       <hr />
--- src/components/pages/producto/FormProducto.jsx
+++ src/components/pages/producto/FormProducto.jsx
@@ -2,9 +2,7 @@ import { Form, Button } from "react-bootstrap";
 
 const FormProducto = () => {
   return (
-    <Form
-      className="my-2 py-1"
-    >
+    <Form className="my-2 py-1">
       <Form.Group className="mb-3" controlId="inputNombre">
         <Form.Label>Producto</Form.Label>
         <Form.Control
@@ -14,16 +12,18 @@ const FormProducto = () => {
           minLength={2}
           maxLength={20}
         />
+        <Form.Text className="text-danger">prueba de error</Form.Text>
       </Form.Group>
       <Form.Group className="mb-3" controlId="inputPrecio">
         <Form.Label>Precio</Form.Label>
         <Form.Control
-          type="text"
+          type="number"
           placeholder="Ej: $1000"
           required
           minLength={3}
           maxLength={15}
         />
+        <Form.Text className="text-danger">prueba de error</Form.Text>
       </Form.Group>
       <Form.Group className="mb-3" controlId="inputURL">
         <Form.Label>URL imagen</Form.Label>
@@ -34,36 +34,44 @@ const FormProducto = () => {
           minLength={5}
           maxLength={100}
         />
+        <Form.Text className="text-danger">prueba de error</Form.Text>
       </Form.Group>
       <Form.Group className="mb-3" controlId="inputCategoria">
         <Form.Label>Categoria</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Ej: Bebidas."
-          required
-          minLength={5}
-          maxLength={20}
-        />
+        <Form.Select aria-label="Default select example">
+          <option>Seleccione una categoria</option>
+          <option value="1">Infusiones</option>
+          <option value="2">Batidos</option>
+          <option value="3">Dulce</option>
+          <option value="4">Salado</option>
+        </Form.Select>
+        <Form.Text className="text-danger">prueba de error</Form.Text>
       </Form.Group>
       <Form.Group className="mb-3" controlId="inputDescBreve">
         <Form.Label>Descripción Breve</Form.Label>
         <Form.Control
+          as="textarea"
+          rows={3}
           type="text"
           placeholder="Ej: Un cafe rico."
           required
           minLength={3}
           maxLength={15}
         />
+        <Form.Text className="text-danger">prueba de error</Form.Text>
       </Form.Group>
       <Form.Group className="mb-3" controlId="inputDescAmplia">
         <Form.Label>Descripción Amplia</Form.Label>
         <Form.Control
+          as="textarea"
+          rows={3}
           type="text"
           placeholder="Ej: Un cafe rico bla bla bla bla bla bla bla..."
           required
           minLength={15}
           maxLength={250}
         />
+        <Form.Text className="text-danger">prueba de error</Form.Text>
       </Form.Group>
       <div className="text-center my-2">
         <Button variant="success" type="submit">
